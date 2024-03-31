5d5ab8b2874f4843718f4a3373594169cbe6103e
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-03-31 03:35:58 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -1,9 +1,5 @@
-if game.PlaceId == 16148053600 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/The%20Upgrade%20Tree%20Of%20Tree.lua",true))()
-  else
-
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Hallow Hub | NO GAME CONNECTED", HidePremium = false, IntroText = "Script By Moligrafi", SaveConfig = false, ConfigFolder = "Hallow Hub"})
 
-end
 OrionLib:Init()
