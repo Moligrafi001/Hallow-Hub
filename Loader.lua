fe903620c40f961f7957e38138ede429c703e246
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-03-31 03:36:41 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
+--
+if game.PlaceId == 16148053600 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/The%20Upgrade%20Tree%20Of%20Tree.lua",true))()
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
+end
