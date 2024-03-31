47e546053364a9089fd21b0ad0128ad46374ab3c
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-03-31 03:26:28 -0300

    Add files via upload

---
+++ HallowHub.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 16148053600 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/The%20Upgrade%20Tree%20Of%20Tree.lua",true))()
+  else
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Hallow Hub | NO GAME CONNECTED", HidePremium = false, IntroText = "Script By Moligrafi", SaveConfig = false, ConfigFolder = "Hallow Hub"})
+
+end
+OrionLib:Init()
