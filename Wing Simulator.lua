42a1de2bedeab83516b532a1dac0a37613b142bb
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-03-31 06:33:00 -0300

    update Wing Simulator.lua

--- Wing Simulator.lua
+++ Wing Simulator.lua
@@ -70,4 +70,27 @@ Menu:AddToggle({
 	end
 })
 
+-- Games
+local Games = Window:MakeTab({
+	Name = "Another Games",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Games:AddSection({
+	Name = "All the available games for script"
+})
+Games:AddButton({
+	Name = "The Upgrade Tree Of Tree",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+Games:AddButton({
+	Name = "Wing Simulator",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+Games:AddLabel("Click to join!")
+
 OrionLib:Init()
