7a4379e8fb44bf9ce5fe2d8997a0e734ea9e2cd0
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-03-31 05:28:19 -0300

    update Wing Simulator.lua

--- Wing Simulator.lua
+++ Wing Simulator.lua
@@ -1,1 +1,18 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/Wing%20Simulator.lua",true))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Hallow Hub | Wing Simulator", HidePremium = false, IntroText = "Script By Moligrafi", SaveConfig = false, ConfigFolder = "Wing Simulator"})
+
+-- Valores
+_G.AutoClick = true
+
+-- Funcoes
+function AutoClick()
+  while _G.AutoClick == true do
+    local args = {
+    [1] = 1
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Click"):FireServer(unpack(args))
+    wait(0.0001)
+   end
+end
