7fec372325e57a1a515d3a7730caa5bd681f93b1
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-02 02:25:33 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -1,6 +1,6 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Hallow Hub | NO GAME CONNECTED", HidePremium = false, IntroText = "Hallow Hub", IntroIcon = 'https://cdn.discordapp.com/attachments/1208977324750536745/1224587306413981797/pumpkin_8745242.png?ex=661e08cd&is=660b93cd&hm=311a038333550879e433a2a976d7f5575ef0f097fa8a225a6ad17bc498bd74bc&', SaveConfig = false, ConfigFolder = "Hallow Hub"})
+local Window = OrionLib:MakeWindow({Name = "Hallow Hub | NO GAME CONNECTED", HidePremium = false, IntroText = "Hallow Hub", SaveConfig = false, ConfigFolder = "Hallow Hub"})
 
 -- Games
 local Games = Window:MakeTab({
