d255f51f06e8778b71fa617fa4e1f6bd82568f42
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-03 23:35:43 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -2,7 +2,8 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Hallow Hub | NO GAME CONNECTED", HidePremium = false, IntroText = "Hallow Hub", SaveConfig = false, ConfigFolder = "Hallow Hub"})
 
--- Função para teletransportar o jogador para um determinado Place ID
+-- Teleport
+local selectedGame = "The Upgrade Tree Of Tree"
 function TeleportToGame(placeId)
     local teleportService = game:GetService("TeleportService")
     local success, errorMessage = pcall(function()
@@ -12,9 +13,11 @@ function TeleportToGame(placeId)
         warn("Erro ao teletransportar: " .. errorMessage)
     end
 end
-
--- Variável para armazenar o jogo selecionado
-local selectedGame = "The Upgrade Tree Of Tree"
+-- Lista de Place IDs correspondentes aos jogos
+local gamePlaceIds = {
+    ["The Upgrade Tree Of Tree"] = 16148053600,
+    ["Wing Simulator"] = 4535346003
+}
 
 -- Games
 local Games = Window:MakeTab({
@@ -22,25 +25,15 @@ local Games = Window:MakeTab({
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
-
--- Lista de Place IDs correspondentes aos jogos
-local gamePlaceIds = {
-    ["The Upgrade Tree Of Tree"] = 16148053600,
-    ["Wing Simulator"] = 4535346003
-}
-
 Games:AddButton({
     Name = "Join Selected Game",
     Callback = function()
         local placeId = gamePlaceIds[selectedGame]
         if placeId then
             TeleportToGame(placeId)
-        else
-            warn("ID do Place não encontrado para o jogo selecionado.")
         end
     end
 })
-
 Games:AddDropdown({
     Name = "Select Game to Join",
     Default = selectedGame,
