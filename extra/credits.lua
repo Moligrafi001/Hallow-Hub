893a5f0fcf2fc400386bb96b00f35e4403aeb13d
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-03 22:44:00 -0300

    update extra/credits.lua

--- extra/credits.lua
+++ extra/credits.lua
@@ -1,25 +1,27 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/credits.lua",true))()
+-- Funcoes
+function TeleportToGame(placeId)
+    local teleportService = game:GetService("TeleportService")
+    local success, errorMessage = pcall(function()
+    end)
+        teleportService:Teleport(placeId)
+end
 
--- Credits
+-- Games
 local Games = Window:MakeTab({
-	Name = "Another Games",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Games:AddSection({
-	Name = "All the available games for script"
+    Name = "Games",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 Games:AddButton({
-	Name = "The Upgrade Tree Of Tree",
-	Callback = function()
-      		print("button pressed")
-  	end    
+    Name = "The Upgrade Tree Of Tree",
+    Callback = function()
+        TeleportToGame(16148053600)
+    end
 })
 Games:AddButton({
-	Name = "Wing Simulator",
-	Callback = function()
-      		print("button pressed")
-  	end    
+    Name = "Wing Simulator",
+    Callback = function()
+        TeleportToGame(4535346003)
+    end
 })
-Games:AddLabel("Click to join!")
-Menu:AddLabel("Instagram: _moligrafi_")
