046efff53439f262fc2fc103237727f6dc196b41
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-03 22:36:19 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -2,15 +2,12 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Hallow Hub | NO GAME CONNECTED", HidePremium = false, IntroText = "Hallow Hub", SaveConfig = false, ConfigFolder = "Hallow Hub"})
 
--- Função para teletransportar o jogador para um determinado Place ID
+-- funcoes
 function TeleportToGame(placeId)
     local teleportService = game:GetService("TeleportService")
     local success, errorMessage = pcall(function()
-        teleportService:Teleport(placeId)
     end)
-    if not success then
-        warn("Erro ao teletransportar: " .. errorMessage)
-    end
+        teleportService:Teleport(placeId)
 end
 
 -- Games
@@ -19,20 +16,16 @@ local Games = Window:MakeTab({
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
-
 Games:AddButton({
     Name = "The Upgrade Tree Of Tree",
     Callback = function()
-        print("Botão The Upgrade Tree Of Tree pressionado")
-        TeleportToGame(16148053600) -- ID do Place de The Upgrade Tree Of Tree
+        TeleportToGame(16148053600)
     end
 })
-
 Games:AddButton({
     Name = "Wing Simulator",
     Callback = function()
-        print("Botão Wing Simulator pressionado")
-        TeleportToGame(4535346003) -- ID do Place do Wing Simulator
+        TeleportToGame(4535346003)
     end
 })
 
