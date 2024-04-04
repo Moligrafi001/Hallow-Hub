64392c5e3708afb8cf5f201468ec1f05512840e4
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-04 18:37:28 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -1,4 +1,4 @@
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))() -- Removido, pois iremos incluir dinamicamente abaixo
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({
   Name = "Hallow Hub | NO GAME CONNECTED", 
@@ -7,6 +7,21 @@ local Window = OrionLib:MakeWindow({
   SaveConfig = false, 
   ConfigFolder = "Hallow Hub"})
 
--- Adicione algo que importe o 'loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/credits.lua",true))()' como uma parte do código.
+-- Insira o código 'extra/credits.lua' aqui:
+local creditsCode = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/credits.lua")
+
+-- Crie um novo ambiente para executar o código, isolando-o e permitindo que você capture o retorno:
+local executeCreditsCode = function()
+    local f, err = loadstring(creditsCode)
+    if f then
+        return f() -- Executa o código e retorna qualquer valor
+    else
+        warn("Erro ao carregar 'credits.lua':", err)
+    end
+end
+
+local creditsModuleResult = executeCreditsCode() -- Receba o possível retorno de credits.lua
+
+-- Aqui você pode usar qualquer função, variável, etc.. definidas dentro do creditsModuleResult, se houver
 
 OrionLib:Init()
