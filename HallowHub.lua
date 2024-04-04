25db299ff82ee4296406605072dc06c3d6fe9b2f
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-03 22:47:36 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -28,5 +28,27 @@ Games:AddButton({
         TeleportToGame(4535346003)
     end
 })
+Games:AddSlider({
+	Name = "Slider",
+	Min = 0,
+	Max = 3,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "The Upgrade Tree Of Tree",
+	Callback = function()
+		TeleportToGame(16148053600)
+	end
+})
+
+--[[
+Name = <string> - The name of the slider.
+Min = <number> - The minimal value of the slider.
+Max = <number> - The maxium value of the slider.
+Increment = <number> - How much the slider will change value when dragging.
+Default = <number> - The default value of the slider.
+ValueName = <string> - The text after the value number.
+Callback = <function> - The function of the slider.
+]]
 
 OrionLib:Init()
