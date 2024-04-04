d4bca2ad8762e949476b70ff6329a6d23e41450b
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-03 23:43:18 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -1,7 +1,18 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
+-- Carregar a biblioteca Orion
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Hallow Hub | NO GAME CONNECTED", HidePremium = false, IntroText = "Hallow Hub", SaveConfig = false, ConfigFolder = "Hallow Hub"})
 
--- Adicione algo que importe o 'loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/credits.lua",true))()' como uma parte do código.
+-- Criar uma janela com Orion
+local Window = OrionLib:MakeWindow({
+    Name = "Hallow Hub | NO GAME CONNECTED",
+    HidePremium = false,
+    IntroText = "Hallow Hub",
+    SaveConfig = false,
+    ConfigFolder = "Hallow Hub"
+})
 
+-- Carregar e executar o conteúdo do arquivo 'credits.lua'
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/credits.lua"))()
+
+-- Inicializar a biblioteca Orion
 OrionLib:Init()
