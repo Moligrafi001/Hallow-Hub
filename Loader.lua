133abad47e552d4aa633fcdc0606662ea82aee08
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-05 16:27:41 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,8 @@ if game.PlaceId == 16148053600 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Upgrade%20Tree%20Of%20Tree.lua",true))()
 elseif game.PlaceId == 4535346003 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Wing%20Simulator.lua",true))()
+elseif game.PlaceId == 6598746935 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Farm%20for%20Fun.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 end
