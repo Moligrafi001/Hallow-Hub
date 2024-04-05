eb5ddb33a7d5c58bd3098fc7ba26171d2918e0f7
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-05 17:26:23 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -7,21 +7,43 @@ local Window = OrionLib:MakeWindow({
   SaveConfig = false, 
   ConfigFolder = "Hallow Hub"})
 
--- Insira o código 'extra/credits.lua' aqui:
-local creditsCode = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/credits.lua")
-
--- Crie um novo ambiente para executar o código, isolando-o e permitindo que você capture o retorno:
-local executeCreditsCode = function()
-    local f, err = loadstring(creditsCode)
-    if f then
-        return f() -- Executa o código e retorna qualquer valor
-    else
-        warn("Erro ao carregar 'credits.lua':", err)
-    end
+-- Teleport
+local selectedGame = "The Upgrade Tree Of Tree"
+function TeleportToGame(placeId)
+    local teleportService = game:GetService("TeleportService")
+    local success, errorMessage = pcall(function()
+        teleportService:Teleport(placeId, game.Players.LocalPlayer)
+    end)
 end
+-- Lista de Place IDs correspondentes aos jogos
+local gamePlaceIds = {
+    ["The Upgrade Tree Of Tree"] = 16148053600,
+    ["Wing Simulator"] = 4535346003,
+    ["Farm for Fun! 🌾"] = 6598746935
+}
 
-local creditsModuleResult = executeCreditsCode() -- Receba o possível retorno de credits.lua
-
--- Aqui você pode usar qualquer função, variável, etc.. definidas dentro do creditsModuleResult, se houver
+-- Credits
+local Credits = Window:MakeTab({
+    Name = "Credits",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+Credits:AddDropdown({
+    Name = "Select Game to Join",
+    Default = selectedGame,
+    Options = {"The Upgrade Tree Of Tree", "Wing Simulator"},
+    Callback = function(value)
+        selectedGame = value
+    end    
+})
+Credits:AddButton({
+    Name = "Join Selected Game",
+    Callback = function()
+        local placeId = gamePlaceIds[selectedGame]
+        if placeId then
+            TeleportToGame(placeId)
+        end
+    end
+})
 
 OrionLib:Init()
