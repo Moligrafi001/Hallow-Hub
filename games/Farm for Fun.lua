8409720ffe51ca61df23f50dadc61d6972d5f2a9
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-05 16:24:38 -0300

    update games/Farm for Fun.lua

---
+++ games/Farm for Fun.lua
@@ -0,0 +1,53 @@
+-- Carregar a biblioteca Orion
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({
+  Name = "Hallow Hub | Farm for Fun! 🌾",
+  HidePremium = false,
+  IntroText = "Hallow Hub",
+  SaveConfig = false,
+  ConfigFolder = "Farm for Fun! 🌾"
+})
+OrionLib:MakeNotification({
+  Name = "Última atualização - 05/04/2024",
+  Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
+  Image = "rbxassetid://4483345998",
+  Time = 9
+})
+
+-- Valores
+_G.AutoHarvest = true
+_G.AutoBuy = true
+
+-- Função para colher automaticamente
+function AutoHarvest()
+  while _G.AutoHarvest do
+    for i = 1, 263 do
+      local crop = "s" .. i
+      local args = {
+        [1] = {
+          [1] = crop
+        }
+      }
+      workspace:WaitForChild("__THINGS"):WaitForChild("__REMOTES"):WaitForChild("harvest crop"):FireServer(unpack(args))
+    end
+    wait(1)
+  end
+end
+
+-- Menu
+local Menu = Window:MakeTab({
+  Name = "Menu",
+  Icon = "rbxassetid://4483345998",
+  PremiumOnly = false
+})
+local Section = Menu:AddSection({
+  Name = "Auto Farm"
+})
+Menu:AddToggle({
+  Name = "Auto Harvest",
+  Default = false,
+  Callback = function(Value)
+    _G.AutoHarvest = Value
+      AutoHarvest()
+    end
+})
