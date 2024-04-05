a749f8c4b5ab9cfc94b6975f9a608ef390bb29c2
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-05 17:24:07 -0300

    update games/Wing Simulator.lua

--- games/Wing Simulator.lua
+++ games/Wing Simulator.lua
@@ -81,28 +81,4 @@ Menu:AddToggle({
 	end
 })
 
--- Credits
-local Games = Window:MakeTab({
-	Name = "Another Games",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Games:AddSection({
-	Name = "All the available games for script"
-})
-Games:AddButton({
-	Name = "The Upgrade Tree Of Tree",
-	Callback = function()
-      		print("button pressed")
-  	end    
-})
-Games:AddButton({
-	Name = "Wing Simulator",
-	Callback = function()
-      		print("button pressed")
-  	end    
-})
-Games:AddLabel("Click to join!")
-Menu:AddLabel("Instagram: _moligrafi_")
-
 OrionLib:Init()
