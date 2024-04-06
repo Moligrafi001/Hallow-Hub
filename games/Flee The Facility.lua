18fe7f76a2eba3442b08bd2fbd665a325d516016
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-06 11:34:26 -0300

    update games/Flee The Facility.lua

--- games/Flee The Facility.lua
+++ games/Flee The Facility.lua
@@ -1,1 +1,32 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Flee%20The%20Facility.lua",true))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({
+  Name = "Hallow Hub | Flee The Facility",
+  HidePremium = false,
+  IntroText = "Hallow Hub",
+  SaveConfig = false,
+  ConfigFolder = "Flee The Facility"})
+OrionLib:MakeNotification({
+	Name = "Última atualização - 06/04/2024",
+	Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
+	Image = "rbxassetid://4483345998",
+	Time = 9
+})
+
+-- Menu
+local Menu = Window:MakeTab({
+	Name = "Menu",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Menu:AddSection({
+	Name = "OP Script"
+})
+Menu:AddButton({
+    Name = "Load Script",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Flee%20The%20Facility.lua",true))()
+    end
+})
+
+OrionLib:Init()
