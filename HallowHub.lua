d4701039ce6c9cfbcaf06487150127a19bbf7f56
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-06 11:33:37 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -1,4 +1,4 @@
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))() -- Removido, pois iremos incluir dinamicamente abaixo
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({
   Name = "Hallow Hub | NO GAME CONNECTED", 
