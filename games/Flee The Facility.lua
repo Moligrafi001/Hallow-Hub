9fcd98acf58ef83b838dd40dd3d7de64cc2cc052
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-06 12:57:24 -0300

    update games/Flee The Facility.lua

--- games/Flee The Facility.lua
+++ games/Flee The Facility.lua
@@ -5,78 +5,79 @@ local Window = OrionLib:MakeWindow({
   HidePremium = false,
   IntroText = "Hallow Hub",
   SaveConfig = false,
-  ConfigFolder = "Flee The Facility"})
+  ConfigFolder = "Flee The Facility"
+})
 OrionLib:MakeNotification({
-	Name = "Última atualização - 06/04/2024",
-	Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
-	Image = "rbxassetid://4483345998",
-	Time = 9
+    Name = "Última atualização - 06/04/2024",
+    Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
+    Image = "rbxassetid://4483345998",
+    Time = 9
 })
 
 -- Valores
 _G.FB = true
 
--- Funcoes
+-- Função para controlar as configurações de iluminação
 function FB()
     while _G.FB do
-    game:GetService("Lighting").Brightness = 2
-    game:GetService("Lighting").ClockTime = 14
-    game:GetService("Lighting").FogEnd = 100000
-    game:GetService("Lighting").GlobalShadows = false
-    game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
-    wait(0.01)
-  end
+        game:GetService("Lighting").Brightness = 2
+        game:GetService("Lighting").ClockTime = 14
+        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").GlobalShadows = false
+        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+        wait(0.01)
+    end
 end
 
 -- Menu
 local Menu = Window:MakeTab({
-	Name = "Menu",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Menu",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 local Section = Menu:AddSection({
-	Name = "OP Script"
+    Name = "OP Script"
 })
 Menu:AddButton({
     Name = "FTFHAX",
     Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/LeviTheOtaku/roblox-scripts/main/FTFHAX.lua",true))()
-  OrionLib:MakeNotification({
-	Name = "FTFHAX executado!",
-	Content = "Ação irreversível, evite executar mais de uma vez.",
-	Image = "rbxassetid://4483345998",
-	Time = 9
-})
+        OrionLib:MakeNotification({
+            Name = "FTFHAX executado!",
+            Content = "Ação irreversível, evite executar mais de uma vez.",
+            Image = "rbxassetid://4483345998",
+            Time = 9
+        })
     end
 })
 Menu:AddButton({
     Name = "Chat Spy",
     Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/ChatSpy.lua",true))()
-  OrionLib:MakeNotification({
-	Name = "Chat Spy executado!",
-	Content = "Ação irreversível, evite executar mais de uma vez.",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
+        OrionLib:MakeNotification({
+            Name = "Chat Spy executado!",
+            Content = "Ação irreversível, evite executar mais de uma vez.",
+            Image = "rbxassetid://4483345998",
+            Time = 5
+        })
     end
 })
 
 -- Misc
 local Misc = Window:MakeTab({
-	Name = "Misc",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Misc",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 local Section = Misc:AddSection({
-	Name = "Useful"
+    Name = "Useful"
 })
 Misc:AddToggle({
-  Name = "Fullbright",
-  Default = false,
-  Callback = function(Value)
-    _G.FB = Value
-      FB()
+    Name = "Fullbright",
+    Default = false,
+    Callback = function(Value)
+        _G.FB = Value
+        FB()
     end
 })
 
