0b44f0864b03e3a665033e41f89963a3306abf07
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-07 16:08:44 -0300

    update extra/BaseScript.lua

--- extra/BaseScript.lua
+++ extra/BaseScript.lua
@@ -1,25 +1,31 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/2%20Player%20Pizza%20Factory%20Tycoon.lua",true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({
-  Name = "Hallow Hub | Nome do Jogo",
+  Name = "Hallow Hub | 2 Player Pizza Factory Tycoon",
   HidePremium = false,
   IntroText = "Hallow Hub",
   SaveConfig = false,
-  ConfigFolder = "Nome do Jogo"})
+  ConfigFolder = "2 Player Pizza Factory Tycoon"})
 OrionLib:MakeNotification({
-	Name = "Última atualização - ??/??/2024",
+	Name = "Última atualização - 07/04/2024",
 	Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
 	Image = "rbxassetid://4483345998",
 	Time = 9
 })
 
 -- Valores
-_G.AlgumaCoisa 
+_G.AlgumaCoisa = false
 
 -- Funcoes
-function AutoClick()
-  while _G.AutoClick == true do
-      print("Hello World!")
-    wait(0.0001)
+function AlgumaCoisa()
+  while _G.AlgumaCoisa == true do
+      OrionLib:MakeNotification({
+	Name = "Teste",
+	Content = "minha bossetinha",
+	Image = "rbxassetid://4483345998",
+	Time = 1
+})
+    wait(5)
    end
 end
 
@@ -39,15 +45,6 @@ Menu:AddToggle({
 		_G.AlgumaCoisa = Value
 		AlgumaCoisa()
 	end
-})
-Menu:AddButton({
-	Name = "Nao sei",
-	Callback = function()
-      print("Goodbye World!")
-  	end
-})
-local Section = Trees:AddSection({
-	Name = "Auto Purchase"
 })
 
 Menu:AddLabel("Instagram: _moligrafi_")
