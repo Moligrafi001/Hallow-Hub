a11a9a2b8509fbfd22ad1267f3988e5c33c8ed74
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-07 15:41:19 -0300

    update games/2 Player Pizza Factory Tycoon.lua

--- games/2 Player Pizza Factory Tycoon.lua
+++ games/2 Player Pizza Factory Tycoon.lua
@@ -1,3 +1,4 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/2%20Player%20Pizza%20Factory%20Tycoon.lua",true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({
   Name = "Hallow Hub | 2 Player Pizza Factory Tycoon",
