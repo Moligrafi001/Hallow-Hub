0933258fe9228ef112ab0107533729f036823d7b
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-07 15:36:42 -0300

    update games/2 Player Pizza Factory Tycoon.lua

---
+++ games/2 Player Pizza Factory Tycoon.lua
@@ -0,0 +1,36 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({
+  Name = "Hallow Hub | Nome do Jogo",
+  HidePremium = false,
+  IntroText = "Hallow Hub",
+  SaveConfig = false,
+  ConfigFolder = "Nome do Jogo"})
+OrionLib:MakeNotification({
+	Name = "Última atualização - ??/??/2024",
+	Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
+	Image = "rbxassetid://4483345998",
+	Time = 9
+})
+
+-- Valores
+_G.AlgumaCoisa
+
+-- Funcoes
+function AutoClick()
+    print("Hello World!")
+  while _G.AutoClick == true do
+    wait(0.0001)
+   end
+end
+
+-- Menu
+local Menu = Window:MakeTab({
+	Name = "Menu",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Menu:AddSection({
+	Name = "Auto Farm"
+})
+
+OrionLib:Init()
