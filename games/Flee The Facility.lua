9dfeb2874cbb503e755ea717b0d97add8962f484
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-08 18:21:39 -0300

    update games/Flee The Facility.lua

--- games/Flee The Facility.lua
+++ games/Flee The Facility.lua
@@ -85,80 +85,6 @@ function SP()
 end
 
 -- ESP System
-local ESP = Instance.new(
-    "Folder",
-    workspace
-) ESP.Name = "ESP"
-local ESPPC = Instance.new(
-    "Folder",
-    workspace
-) ESPPC.Name = "ESPComputer"
-function GetSizeOfObject(Obj)
-    if Obj:IsA("BasePart") then
-        return Obj.Size
-    elseif Obj:IsA("Model") then
-        return Obj:GetExtentsSize()
-    end
-end
-function CreateESPPart(BodyPart,r,g,b)
-    local ESPPartparent = BodyPart
-    local Box = Instance.new("BoxHandleAdornment")
-    Box.Size = GetSizeOfObject(ESPPartparent) + Vector3.new(0.1, 0.1, 0.1)
-    Box.Name = "ESPPart"
-    Box.Adornee = ESPPartparent
-    Box.Color3 = Color3.fromRGB(r,g,b)
-    Box.AlwaysOnTop = true
-    Box.ZIndex = 5
-    Box.Transparency = 0.4
-    Box.Parent = ESP
-    if BodyPart.Parent.Name == game.Players.LocalPlayer.Name then
-        Box:remove()
-        spawn (function()
-            while true do
-                wait(0.1)
-                if BodyPart ~= nil then
-                    Box:remove()
-                end
-            end
-        end)
-    end
-end
-local OldNameCall = nil
-OldNameCall = hookmetamethod(game, "__namecall", function(...)
-    local Args = {...}
-    local Self = Args[1]
-    if getnamecallmethod() == "FireServer" and tostring(Self) == "RemoteEvent" and Args[1] == "ReportPhysicsFPS" then
-        return wait(math.huge)
-    end
-    return OldNameCall(...)
-end)
-local BeastColor = Color3.new(255, 0, 0)
-local InoccentColor = Color3.new(0, 200, 0)
-local CreateEsp = function()
-  for _, v in pairs(game:GetService('Players'):GetChildren()) do
-    if v.Name ~= game:GetService('Players').LocalPlayer.Name then
-      local Esp = Instance.new('Highlight', v.Character)
-      Esp.Name = 'EspPlayer'
-      Esp.FillTransparency = 0.5
-    end
-  end
-end
-local UpdateEsp = function()
-  for _, v in pairs(game:GetService('Players'):GetChildren()) do
-    if v.Name ~= game:GetService('Players').LocalPlayer.Name then
-      if v.Character:findFirstChild('BeastPowers') then
-        v.Character.EspPlayer.FillColor = BeastColor
-      else
-        v.Character.EspPlayer.FillColor = InoccentColor
-      end
-    end
-  end
-end
-local Map = workspace:FindFirstChild(tostring(game.ReplicatedStorage.CurrentMap.Value))
-local plr = game:GetService("Players").LocalPlayer
-local chr = plr.Character
-local hum = chr.Humanoid
-local hrp = chr.HumanoidRootPart
 
 -- Invade Match System
 local function TeleportToCoordinates()
@@ -205,24 +131,11 @@ local Section = Menu:AddSection({
 	Name = "·-–— ESP —–-·"
 })
 Menu:AddToggle({
-	Name = "Player ESP",
+	Name = "Computer ESP",
 	Default = false,
-	Flag = "Toggle1",
-   Callback = function(state)
-       PlayerEsp = state
-       if PlayerEsp then
-          CreateEsp()
-          while wait() and PlayerEsp do
-            UpdateEsp()
-          end
-       else
-         for _, x in pairs(workspace:GetDescendants()) do
-           if x:IsA('Highlight') and x.Name == 'EspPlayer' then
-             x:Destroy()
-           end
-         end
-       end
-   end,
+	Callback = function(Value)
+		print(Value)
+	end    
 })
 Menu:AddToggle({
 	Name = "Computer ESP",
