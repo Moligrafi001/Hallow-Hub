7971780d0ef3eb2a942425682cd71c5ea8a7117d
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-14 17:07:22 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,6 +7,8 @@ elseif game.PlaceId == 6598746935 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Farm%20for%20Fun.lua",true))()
 elseif game.PlaceId == 893973440 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Flee%20The%20Facility.lua",true))()
+elseif game.PlaceId == 12684368460 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Build%20Bunker%20To%20Survive.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 end
