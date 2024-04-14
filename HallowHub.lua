20ce6bdf09a748cb7ddd418f8ee1506be25d4e61
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-14 19:47:44 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -22,7 +22,8 @@ local gamePlaceIds = {
     ["Wing Simulator"] = 4535346003,
     ["Farm for Fun! 🌾"] = 6598746935,
     ["Flee The Facility"] = 893973440,
-    ["Build a bunker in your house to survive zombies"] = 12684368460
+    ["Build a bunker in your house to survive zombies"] = 12684368460,
+    ["Planet Clicker 🌎"] = 6428116890
 }
 
 -- Credits
@@ -34,7 +35,7 @@ local Credits = Window:MakeTab({
 Credits:AddDropdown({
     Name = "Select Game to Join",
     Default = selectedGame,
-    Options = {"The Upgrade Tree Of Tree", "Wing Simulator", "Farm for Fun! 🌾", "Flee The Facility", "Build a bunker in your house to survive zombies"},
+    Options = {"The Upgrade Tree Of Tree", "Wing Simulator", "Farm for Fun! 🌾", "Flee The Facility", "Build a bunker in your house to survive zombies", "Planet Clicker 🌎"},
     Callback = function(value)
         selectedGame = value
     end    
