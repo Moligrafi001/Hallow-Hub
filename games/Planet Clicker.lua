b70d18c1a0ae6a6359c545b93c3b8a18b4b9c0a2
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-14 20:20:34 -0300

    update games/Planet Clicker.lua

--- games/Planet Clicker.lua
+++ games/Planet Clicker.lua
@@ -1,4 +1,3 @@
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Planet%20Clicker.lua",true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/thanhdat4461/OrionMoblie/main/source')))()
 local Window = OrionLib:MakeWindow({
     Name = "Hallow Hub | Planet Clicker 🌎",
@@ -32,19 +31,6 @@ function AutoRebirth()
 		end
 end
 
--- Auto Research
-_G.AutoResearch = false
-function AutoResearch()
-		while _G.AutoResearch == true do
-			local args = {
-    	[1] = game:GetService("Players").LocalPlayer.PlayerGui.ResearchGui.Frame.ScrollingFrame.Research1
-}
-
-			game:GetService("ReplicatedStorage").Research:InvokeServer(unpack(args))
-			wait(1)
-		end
-end
-
 -- Menu
 local Menu = Window:MakeTab({
     Name = "Menu",
@@ -68,24 +54,6 @@ Menu:AddToggle({
     Callback = function(Value)
       _G.AutoRebirth = Value
       AutoRebirth()
-    end
-})
-
--- Buy
-local Buy = Window:MakeTab({
-    Name = "Buy",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
-local Section = Buy:AddSection({
-    Name = "Auto Buy"
-})
-Buy:AddToggle({
-    Name = "Auto Research",
-    Default = false,
-    Callback = function(Value)
-      _G.AutoResearch = Value
-      AutoResearch()
     end
 })
 
