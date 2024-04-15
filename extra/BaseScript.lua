6873fb97318ec5de9c20d6d63291b93100485d2a
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-14 23:35:58 -0300

    update extra/BaseScript.lua

--- extra/BaseScript.lua
+++ extra/BaseScript.lua
@@ -11,33 +11,33 @@ OrionLib:MakeNotification({
 	Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
 	Image = "rbxassetid://4483345998",
 	Time = 9
-})
+}) -- Notificação
 
--- Valores
-_G.AlgumaCoisa = false
-function AlgumaCoisa()
-  while _G.AlgumaCoisa == true do
-      Aqui o codigo copiado do simple spy
-    wait(5)
-   end
-end
+-- Auto Farm
+_G.AutoFarm = false -- Define a variável como inativa
+function AutoFarm() -- Cria uma função
+  while _G.AlgumaCoisa == true do -- Enquanto a variável estiver ativa fará o seguinte loop
+      -- Cole o código remoto que coleto do Simple Spy nessa linha
+    wait(0.0001) -- Repete 1.000 vezes por segundo
+  end -- Sinaliza o fim do loop
+end -- Sinaliza o fim da função
 
 -- Menu
 local Menu = Window:MakeTab({
 	Name = "Menu",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
-})
+}) -- Cria uma guia
 local Section = Menu:AddSection({
 	Name = "Auto Farm"
-})
+}) -- Cria uma sessão
 Menu:AddToggle({
-	Name = "Loop Print",
-	Default = false,
+	Name = "Loop Print", -- Nome do botão
+	Default = false, -- Executa com a opção desativada
 	Callback = function(Value)
-		_G.AlgumaCoisa = Value
-		AlgumaCoisa()
-	end
-})
+		_G.AlgumaCoisa = Value -- Inverte a variável
+		AlgumaCoisa() -- Chama a função após mudar inverter a variável
+	end -- Sinaliza o fim do function
+}) -- Cria um botão
 
-OrionLib:Init()
+OrionLib:Init() -- Obrigatório para finalizar o código
