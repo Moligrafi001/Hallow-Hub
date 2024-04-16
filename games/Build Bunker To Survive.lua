dce96ce57f17f35c08bd4cc893e16039318901b6
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-04-16 14:15:45 -0300

    update games/Build Bunker To Survive.lua

--- games/Build Bunker To Survive.lua
+++ games/Build Bunker To Survive.lua
@@ -4,13 +4,14 @@ local Window = OrionLib:MakeWindow({
     Name = "Hallow Hub | Build Bunker To Survive",
     HidePremium = false,
     IntroText = "Hallow Hub",
-    SaveConfig = false,
+    SaveConfig = false, 
+    IntroIcon = "rbxassetid://17091459839",
     ConfigFolder = "Build Bunker To Survive"
 })
 OrionLib:MakeNotification({
     Name = "Última atualização - 14/04/2024",
     Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
-    Image = "rbxassetid://4483345998",
+    Image = "rbxassetid://17091459839",
     Time = 9
 })
 
