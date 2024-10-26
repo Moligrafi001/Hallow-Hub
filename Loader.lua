0b9832b4218a26fa3b5c87f026f12281213f35b7
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-10-25 21:40:01 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,20 +1,6 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/Loader.lua",true))()
-if game.PlaceId == 16148053600 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Upgrade%20Tree%20Of%20Tree.lua",true))()
-elseif game.PlaceId == 4535346003 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Wing%20Simulator.lua",true))()
-elseif game.PlaceId == 6598746935 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Farm%20for%20Fun.lua",true))()
-elseif game.PlaceId == 893973440 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Flee%20The%20Facility.lua",true))()
-elseif game.PlaceId == 12684368460 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Build%20Bunker%20To%20Survive.lua",true))()
-elseif game.PlaceId == 6428116890 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Planet%20Clicker.lua",true))()
-elseif game.PlaceId == 3101667897 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Legends%20Of%20Speed.lua",true))()
-elseif game.PlaceId == 10824616460 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Sword%20Slasher.lua",true))()
+if game.PlaceId == 537413528 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Build%20a%20Boat%20for%20the%20Treasure.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 end
