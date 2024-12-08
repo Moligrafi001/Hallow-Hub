ada63e5179469aa97f25e8aaf09422810fbac597
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-08 11:46:48 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,8 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/Loader.lua",true))()
 if game.PlaceId == 537413528 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Build%20a%20Boat%20for%20the%20Treasure.lua",true))()
+elseif game.PlaceId == 16148053600 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Upgrade%20Tree%20Of%20Tree.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 end
