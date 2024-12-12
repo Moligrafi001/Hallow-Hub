94a694c5aa4398b95bdf35320931915f0012a2a6
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-12 14:31:27 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -21,7 +21,8 @@ end
 local gamePlaceIds = {
     ["The Upgrade Tree Of Tree"] = 16148053600,
     ["Everything Upgrade Tree"] = 122809141833750,
-    ["Snow Plow Simulator"] = 11701792069
+    ["Snow Plow Simulator"] = 11701792069,
+    ["Farm for Fun! 🌾"] = 6598746935
 }
 
 -- Menu
@@ -37,7 +38,7 @@ local Section = Games:CreateSection("Join game system")
 local Label = Games:CreateLabel("Here are all the supported games in this script <3", "smile")
 local Dropdown = Games:CreateDropdown({
    Name = "Select Game to Join",
-   Options = {"The Upgrade Tree Of Tree", "Everything Upgrade Tree", "Snow Plow Simulator"},
+   Options = {"The Upgrade Tree Of Tree", "Everything Upgrade Tree", "Snow Plow Simulator", "Farm for Fun! 🌾"},
    CurrentOption = {"Choose one"},
    MultipleOptions = false,
    Callback = function(Options)
