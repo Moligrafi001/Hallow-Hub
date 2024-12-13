0140ea6b3c40cfbcf1487ee2d33283d4159e9a12
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-13 14:17:07 -0300

    update games/Farm for Fun.lua

--- games/Farm for Fun.lua
+++ games/Farm for Fun.lua
@@ -175,8 +175,18 @@ local Toggle =  Menu:CreateToggle({
    end,
 })
 
--- Credits
+-- Credits.
 local Credits = Window:CreateTab("Credits", "info")
 local Section = Credits:CreateSection("Creator")
 local Label = Credits:CreateLabel("Discord: moligrafi", "at-sign")
-local Label = Credits:CreateLabel("You can contact me to suggest something or even to ask for a game script")
+local Section = Credits:CreateSection("Co-Dev")
+local Label = Credits:CreateLabel("Discord: world_star_hacker", "at-sign")
+local Section = Credits:CreateSection("Discord Server")
+local Label = Credits:CreateLabel("https://discord.gg/AESCuek87s")
+local Button = Credits:CreateButton({
+   Name = "Copy Server Link",
+   Callback = function()
+   	setclipboard("https://discord.gg/AESCuek87s")
+   	toclipboard("https://discord.gg/AESCuek87s")
+    end,
+})
