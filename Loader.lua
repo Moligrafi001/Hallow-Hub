38c071eda335c6d862aad7ee76ab425610cb420d
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-16 08:59:15 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -17,6 +17,8 @@ elseif game.PlaceId == 81257648942232 then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Slash%20Mobs%20Simulator.lua",true))()
 elseif game.PlaceId == 101162558216961 then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Rune%20INC.lua",true))()
+elseif game.PlaceId == 109819539837829 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Make%20and%20Sell%20Cars.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 end
