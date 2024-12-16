ac55220ee806c6fd2edcc031cb4fe2047aa796d5
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-16 15:26:50 -0300

    update games/Find The Buttons.lua

--- games/Find The Buttons.lua
+++ games/Find The Buttons.lua
@@ -1,7 +1,7 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20The%20Buttons.lua",true))()
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-   Name = "Hallow Hub | Find the Buttons! mag_rightred_circle",
+   Name = "Hallow Hub | Find the Buttons! 🔎🔴",
    Icon = 17091459839,
    LoadingTitle = "Hallow Hub",
    LoadingSubtitle = "By Moligrafi",
@@ -9,12 +9,26 @@ local Window = Rayfield:CreateWindow({
 })
 
 -- Locals
-local AlmostSelected = 39
-local SelectedLevel = Almost Selected - 1
+local SelectedLevel = 39
 
 -- Functions
 local function FireLevel()
-    game:GetService("ReplicatedStorage").Packages.Knit.Services.SStage.RE.NextStageR:FireServer(SelectedLevel)
+	local Stage = SelectedLevel - 1
+	if Stage >= -1 and Stage <= 40 then
+		game:GetService("ReplicatedStorage").Packages.Knit.Services.SStage.RE.NextStageR:FireServer(Stage)
+	end
+end
+local function GiveSpinW()
+	while _G.GiveSpinW == true do
+		game:GetService("ReplicatedStorage").Packages.Knit.Services.SSpin.RE.GiveSpinR:FireServer()
+		wait(0.33)
+	end
+end
+local function SpinW()
+	while _G.SpinW == true do
+		game:GetService("ReplicatedStorage").Packages.Knit.Services.SSpin.RF.Spin:InvokeServer()
+		wait(5.5)
+	end
 end
 
 -- Menu
@@ -34,6 +48,23 @@ local Button = Menu:CreateButton({
    Callback = function()
        FireLevel()
     end,
+})
+local Section = Menu:CreateSection("Spin Wheel")
+local Toggle =  Menu:CreateToggle({
+   Name = "Auto Give Spin",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.GiveSpinW = Value
+   	GiveSpinW()
+   end,
+})
+local Toggle =  Menu:CreateToggle({
+   Name = "Auto Spin Wheel",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.SpinW = Value
+   	SpinW()
+   end,
 })
 
 -- Credits.
