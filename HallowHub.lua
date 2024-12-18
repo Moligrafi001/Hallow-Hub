75b0378131415f9f57b4f5da3b12d77d597ccba5
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-18 01:04:11 -0300

    update HallowHub.lua

--- HallowHub.lua
+++ HallowHub.lua
@@ -22,7 +22,14 @@ local gamePlaceIds = {
     ["The Upgrade Tree Of Tree"] = 16148053600,
     ["Everything Upgrade Tree"] = 122809141833750,
     ["Snow Plow Simulator"] = 11701792069,
-    ["Farm for Fun! 🌾"] = 6598746935
+    ["Farm for Fun! 🌾"] = 6598746935,
+    ["⚔️ Slash Mobs Simulator"] = 81257648942232,
+    ["Rune Inc"] = 101162558216961,
+    ["Make and Sell Cars"] = 109819539837829,
+    ["Find The Buttons! 🔎🔴"] = 91314495602934,
+    ["🥊Punch Monsters!"] = 8034886758,
+    ["Cash Incremental"] = 129159449618378,
+    ["Legends Of Speed"] = 3101667897
 }
 
 -- Credits.
@@ -47,11 +54,11 @@ local Section = Games:CreateSection("Join game system")
 local Label = Games:CreateLabel("Here are all the supported games in this script <3", "smile")
 local Dropdown = Games:CreateDropdown({
    Name = "Select Game to Join",
-   Options = {"The Upgrade Tree Of Tree", "Everything Upgrade Tree", "Snow Plow Simulator", "Farm for Fun! 🌾"},
+   Options = {"The Upgrade Tree Of Tree", "Everything Upgrade Tree", "Snow Plow Simulator", "Farm for Fun! 🌾", "⚔️ Slash Mobs Simulator", "Rune Inc", "Make and Sell Cars", "Find The Buttons! 🔎🔴", "🥊Punch Monsters!", "Cash Incremental"},
    CurrentOption = {"Choose one"},
    MultipleOptions = false,
    Callback = function(Options)
-   		selectedGame = CurrentOption
+   		selectedGame = Options[1]
    end,
 })
 local Button = Games:CreateButton({
