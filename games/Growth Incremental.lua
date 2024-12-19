f372a604b8a9c866f8c9010cd8b7a3ec638e4848
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-19 11:48:56 -0300

    update games/Growth Incremental.lua

---
+++ games/Growth Incremental.lua
@@ -0,0 +1,48 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Growth%20Incremental.lua",true))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | 📈 Growth Incremental!",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
+})
+
+-- Valores
+_G.AutoXP = false
+
+-- Funções
+local function AutoXP()
+	while _G.AutoXP == true do
+		game:GetService("ReplicatedStorage").HandleXP:FireServer("XP", 999999999)
+		wait(1)
+	end
+end
+
+-- Menu
+local Menu = Window:CreateTab("Main", "home")
+local Section = Menu:CreateSection("Upgrade")
+local Toggle =  Menu:CreateToggle({
+   Name = "Auto Buy Upgrades",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.AutoUpgrade = Value
+   	AutoUpgrade()
+   end,
+})
+
+-- Credits.
+local Credits = Window:CreateTab("Credits", "info")
+local Section = Credits:CreateSection("Creator")
+local Label = Credits:CreateLabel("Discord: moligrafi", "at-sign")
+local Section = Credits:CreateSection("Co-Dev")
+local Label = Credits:CreateLabel("Discord: world_star_hacker", "at-sign")
+local Section = Credits:CreateSection("Discord Server")
+local Label = Credits:CreateLabel("discord.gg/AESCuek87s")
+local Button = Credits:CreateButton({
+   Name = "Copy Server Link",
+   Callback = function()
+   	setclipboard("discord.gg/AESCuek87s")
+   	toclipboard("discord.gg/AESCuek87s")
+    end,
+})
