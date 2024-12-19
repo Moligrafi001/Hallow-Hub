80be33d7b652ac3edfdc28701f0582d446e2a188
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-19 12:24:40 -0300

    update games/Growth Incremental.lua

--- games/Growth Incremental.lua
+++ games/Growth Incremental.lua
@@ -50,6 +50,7 @@ local function LPsUpgrades()
 		game:GetService("ReplicatedStorage").UpgradeEvent:FireServer(10, "LP", "Oupg", "OrbRange", 5, true, false, false, true, 3)
 		wait(0.33)
 	end
+	print("Atualizado!")
 end
 
 -- Menu
