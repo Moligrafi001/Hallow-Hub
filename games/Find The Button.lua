bca8e7dc9fda51b453ca3cbce25281972ed18cde
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-20 12:21:17 -0300

    update games/Find The Button.lua

--- games/Find The Button.lua
+++ games/Find The Button.lua
@@ -1,3 +1,4 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20The%20Button.lua",true))()
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Hallow Hub | Find The Button",
