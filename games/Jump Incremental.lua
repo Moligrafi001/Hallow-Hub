54f6e29061e23339392be5c60466e9a6d98abf10
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-21 12:00:01 -0300

    update games/Jump Incremental.lua

--- games/Jump Incremental.lua
+++ games/Jump Incremental.lua
@@ -1,4 +1,4 @@
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Ultimate%20Upgrade%20Tree.lua",true))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Jump%20Incremental.lua",true))()
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Hallow Hub | Jump Incremental",
@@ -8,36 +8,72 @@ local Window = Rayfield:CreateWindow({
    Theme = "Amethyst"
 })
 
+-- Valores
+_G.GetJump = false
+_G.GetMoney = false
 
-game:GetService("ReplicatedStorage").JumpEvent:FireServer()
+-- Funções
+local function Getsiys()
+	while _G.GetJump == true do
+		game:GetService("ReplicatedStorage").JumpEvent:FireServer()
+		wait(0.01)
+	end
+	while _G.GetMoney == true do
+		local args = {
+    [1] = Vector3.new(12.850000381469727, 1.9500000476837158, -24)
+		}
 
-local args = {
+		game:GetService("ReplicatedStorage").CashPileCollected:FireServer(unpack(args))
+		wait(0.01)
+	end
+	while _G.AutoBuy == true do
+		for numero 1, 30 do
+			local final = "n" .. numero
+			game:GetService("ReplicatedStorage").PurchaseEvent:FireServer(final)
+		end
+		wait(1)
+	end
+end
+
+-- Menu
+local Menu = Window:CreateTab("Main", "home")
+local Section = Menu:CreateSection("Auto Farm")
+local Toggle =  Menu:CreateToggle({
+   Name = "Get Money",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.GetMoney = Value
+   	Getsiys()
+   end,
+})
+local Toggle =  Menu:CreateToggle({
+   Name = "Get Jumps",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.GetJump = Value
+   	Getsiys()
+   end,
+})
+local Section = Menu:CreateSection("Upgrade")
+local Toggle =  Menu:CreateToggle({
+   Name = "Auto Buy Upgrades",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.AutoBuy = Value
+   	Getsiys()
+   end,
+})
+local Button = Menu:CreateButton({
+   Name = "Inf Food",
+   Callback = function()
+   	local args = {
     [1] = {
         ["rewardType"] = "Food",
         ["tier"] = 1,
         ["amount"] = math.huge
     }
-}
+		}
 
-game:GetService("ReplicatedStorage").SpawnGiftEvent:FireServer(unpack(args))
-
-while true do
-for numero = 1, 30 do
-local final = "n" .. numero
-local args = {
-    [1] = final
-}
-
-game:GetService("ReplicatedStorage").PurchaseEvent:FireServer(unpack(args))
-end
-wait(0.5)
-end
-
-while true do
-local args = {
-    [1] = Vector3.new(12.850000381469727, 1.9500000476837158, -24)
-}
-
-game:GetService("ReplicatedStorage").CashPileCollected:FireServer(unpack(args))
-wait(0.01)
-end
+		game:GetService("ReplicatedStorage").SpawnGiftEvent:FireServer(unpack(args))
+    end,
+})
