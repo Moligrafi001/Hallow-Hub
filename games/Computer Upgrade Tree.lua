67464670f47a5680b7d25e105ad2ac78bf70682f
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-22 20:13:00 -0300

    update games/Computer Upgrade Tree.lua

--- games/Computer Upgrade Tree.lua
+++ games/Computer Upgrade Tree.lua
@@ -1,10 +1,22 @@
-while true do
-	for _, upgrade in pairs(workspace.Upgrades:GetChildren()) do
-		if upgrade:FindFirstChild("Bough") and upgrade:FindFirstChild("Visible") then
-			if upgrade.Bought.Value == false and upgrade.Visible.Value == true then
-				fireclickdetector(upgrade.ClickDetector)
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Computer%20Upgrade%20Tree.lua",true))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | Find Buttons!👀",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
+})
+
+local function AutoBuy()
+	while _G.AutoBuy == true do
+		for _, upgrade in pairs(workspace.Upgrades:GetChildren()) do
+			if upgrade:FindFirstChild("Bought") and upgrade:FindFirstChild("Visible") then
+				if upgrade.Bought.Value == false and upgrade.Visible.Value == true then
+						fireclickdetector(upgrade.ClickDetector)
+				end
 			end
 		end
+		wait(1)
 	end
-	wait(1)
 end
