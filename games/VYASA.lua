a3ada2b017e375625ee3f40714e213020720580a
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-22 12:15:41 -0300

    update games/VYASA.lua

--- games/VYASA.lua
+++ games/VYASA.lua
@@ -1,7 +1,3 @@
-game:GetService("Players").LocalPlayer.PlayerGui.PlayerMain.Abilities.E:GetChildren()[4].RemoteEvent:FireServer()
-game:GetService("Players").LocalPlayer.PlayerGui.PlayerMain.Abilities.Q:GetChildren()[4].RemoteEvent:FireServer()
-game:GetService("Players").LocalPlayer.PlayerGui.SwordGUI.Frame.GiveGold:FireServer(math.huge)
-
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Clicking%20Quest.lua",true))()
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
@@ -10,4 +6,24 @@ local Window = Rayfield:CreateWindow({
    LoadingTitle = "Hallow Hub",
    LoadingSubtitle = "By Moligrafi",
    Theme = "Amethyst"
+})
+
+-- Valores
+game:GetService("Players").LocalPlayer.PlayerGui.PlayerMain.Abilities.E:GetChildren()[4].RemoteEvent:FireServer()
+game:GetService("Players").LocalPlayer.PlayerGui.PlayerMain.Abilities.Q:GetChildren()[4].RemoteEvent:FireServer()
+game:GetService("Players").LocalPlayer.PlayerGui.SwordGUI.Frame.GiveGold:FireServer(math.huge)
+
+-- Funções
+k
+
+-- Menu
+local Menu = Window:CreateTab("Main", "home")
+local Section = Menu:CreateSection("Auto Farm")
+local Toggle =  Menu:CreateToggle({
+   Name = "Auto Click",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.AutoClick = Value
+   	AutoClick()
+   end,
 })
