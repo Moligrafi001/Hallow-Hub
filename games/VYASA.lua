0ecebc8ee78f461574a1b490580cdadcfadeab40
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-22 12:10:16 -0300

    update games/VYASA.lua

---
+++ games/VYASA.lua
@@ -0,0 +1,4 @@
+game:GetService("Players").LocalPlayer.PlayerGui.PlayerMain.Abilities.E:GetChildren()[4].RemoteEvent:FireServer()
+game:GetService("Players").LocalPlayer.PlayerGui.PlayerMain.Abilities.Q:GetChildren()[4].RemoteEvent:FireServer()
+game:GetService("Players").LocalPlayer.PlayerGui.SwordGUI.Frame.GiveGold:FireServer(math.huge)
+
