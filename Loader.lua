0e64400b8d62f5d26f0e5897bab6a804d53dc137
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-23 11:38:24 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -47,6 +47,8 @@ elseif game.PlaceId == 18242944461 then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Computer%20Upgrade%20Tree.lua",true))()
 elseif game.PlaceId == 105628647191901 then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Swords%20Battle%20Simulator.lua",true))()
+elseif game.PlaceId == 11402943018 then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Ultimate%20Upgrade%20Tree%20Teamwork.lua",true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
 end
