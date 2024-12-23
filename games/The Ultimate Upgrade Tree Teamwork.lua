85daa8d993218eb5bc6fab83796da3b3e0dab75d
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-23 11:21:37 -0300

    update games/The Ultimate Upgrade Tree Teamwork.lua

--- games/The Ultimate Upgrade Tree Teamwork.lua
+++ games/The Ultimate Upgrade Tree Teamwork.lua
@@ -1,3 +1,13 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Swords%20Battle%20Simulator.lua",true))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | The Ultimate Upgrade Tree: Teamwork",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
+})
+
 for _, button in pairs(workspace.Buttons.PointUpgrades:GetChildren()) do
 	if button.Bought.Value == false then
 		fireclickdetector(button.ClickDetector)
