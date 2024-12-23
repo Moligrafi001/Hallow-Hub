048ffd1757d9bd27f78116be15b4690cfc47358c
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-23 19:09:44 -0300

    update games/Dungeons Of Doom.lua

--- games/Dungeons Of Doom.lua
+++ games/Dungeons Of Doom.lua
@@ -1,14 +1,18 @@
-fireproximityprompt(workspace.MonsterLootbag.MonsterLootbag.ProximityPrompt)
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20Buttons.lua",true))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | Dungeons Of Doom",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
+})
 
-fireproximityprompt(workspace.BossLootbag.BossLootbag.ProximityPrompt)
+-- Valores
+_G.AutoMobLoot = false
+_G.AutoCrateLoot = false
 
-fireproximityprompt(workspace.CrateLootbag.CrateLootbag.ProximityPrompt)
-
-workspace.T1Chest.Top.Baseplate.ProximityPrompt
-
-workspace.SpikeTrap.Union.TouchInterest
-
-_G.AutoMobLoot = true
+-- Funções
 local function AutoMobLoot()
 	while _G.AutoMobLoot == true do
 		local playerPosition = workspace.Moligrafi.HumanoidRootPart.Position
@@ -27,4 +31,41 @@ local function AutoMobLoot()
 		wait(0.1)
 	end
 end
-AutoMobLoot()
+local function AutoCrateLoot()
+	while _G.AutoCrateLoot == true do
+		local playerPosition = workspace.Moligrafi.HumanoidRootPart.Position
+		for _, bag in pairs(workspace:GetChildren()) do
+			if bag.Name == "CrateLootbag" then
+				if bag:FindFirstChild("CrateLootbag") then
+					local blockPosition = bag.CrateLootbag.Position
+					local distance = (playerPosition - blockPosition).Magnitude
+					if distance <= 9 then
+						fireproximityprompt(bag.CrateLootbag.ProximityPrompt)
+						print("Apertei!")
+					end
+				end
+			end
+		end
+		wait(0.1)
+	end
+end
+
+-- Menu
+local Menu = Window:CreateTab("Main", "home")
+local Section = Menu:CreateSection("Loot")
+local Toggle =  Menu:CreateToggle({
+   Name = "Collect Mob Loot",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.AutoMobLoot = Value
+   	AutoMobLoot()
+   end,
+})
+local Toggle =  Menu:CreateToggle({
+   Name = "Collect Crate Loot",
+   CurrentValue = false,
+   Callback = function(Value)
+   	_G.AutoCrateLoot = Value
+   	AutoCrateLoot()
+   end,
+})
