4d8c1b867e70af29c8b54ac53c96ebaee6129b76
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-23 12:04:42 -0300

    update games/The Ultimate Upgrade Tree Teamwork.lua

--- games/The Ultimate Upgrade Tree Teamwork.lua
+++ games/The Ultimate Upgrade Tree Teamwork.lua
@@ -1,4 +1,4 @@
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Ultimate%20Upgrade%20Tree%20Teamwork.lua",true))()
+----- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Ultimate%20Upgrade%20Tree%20Teamwork.lua",true))()
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Hallow Hub | The Ultimate Upgrade Tree: Teamwork",
