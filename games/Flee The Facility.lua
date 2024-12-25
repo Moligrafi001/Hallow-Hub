4878db3b4ce1f759285a83790c1e5855b46aa0af
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-25 15:37:07 -0300

    update games/Flee The Facility.lua

--- games/Flee The Facility.lua
+++ games/Flee The Facility.lua
@@ -1,266 +1,53 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Flee%20The%20Facility.lua",true))()
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/thanhdat4461/OrionMoblie/main/source')))()
-local Window = OrionLib:MakeWindow({
-    Name = "Hallow Hub | Flee The Facility",
-    HidePremium = false,
-    IntroText = "Hallow Hub",
-    SaveConfig = false,
-    IntroIcon = "rbxassetid://17091459839",
-    ConfigFolder = "Flee The Facility"
-})
-OrionLib:MakeNotification({
-    Name = "Última atualização - 06/04/2024",
-    Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
-    Image = "rbxassetid://17091459839",
-    Time = 9
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | Flee The Facility",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
 })
 
--- Locals
-local character = game.Players.LocalPlayer.character
-local humanoid = character
-local rotationSpeed = 64
-local userInputService = game:GetService("UserInputService")
-local disconnectIJ = nil
-local walkSpeed = 16
+-- Valores
+_G.ComputerESP = false
 
--- Walk Speed System
-_G.ChangeWalkSpeed = false
-function WalkSpeedLoop()
-    while _G.ChangeWalkSpeed do
-        game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid").WalkSpeed = walkSpeed
-        wait(0.1)
-    end
-end
-
--- Fullbright System
-_G.FB = false
-function FB()
-    while _G.FB do
-        game:GetService("Lighting").Brightness = 2
-        game:GetService("Lighting").ClockTime = 14
-        game:GetService("Lighting").FogEnd = 100000
-        game:GetService("Lighting").GlobalShadows = false
-        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
-        wait(0.01)
-    end
-    game:GetService("Lighting").Brightness = 1
-    game:GetService("Lighting").ClockTime = 14
-    game:GetService("Lighting").FogEnd = 10000000
-    game:GetService("Lighting").GlobalShadows = true
-    game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(200, 200, 200)
-end
-
--- No Clip System
-_G.NC = false
-function NC()
-    while _G.NC == true do
-        for _, part in ipairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-            if part:IsA("BasePart") then
-                if _G.NC then
-                    part.CanCollide = false
-                else
-                    part.CanCollide = true
-                end
-            end
-        end
-        wait(0.1)
-    end
-end
-
--- Anti Slow System
-_G.AS = false
-function AS()
-    while _G.AS do
-        if character then
-            local humanoid = character:FindFirstChildOfClass("Humanoid")
-            if humanoid then
-                if humanoid.WalkSpeed < 16 then
-                    humanoid.WalkSpeed = 16
-                end
-            end
-        end
-        wait(0.01)
-    end
-end
-
--- Spin System
-_G.SP = false
-function SP()
-    while _G.SP do
-        if character then
-            local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
-            if humanoidRootPart then
-                humanoidRootPart.CFrame = humanoidRootPart.CFrame * CFrame.Angles(0, math.rad(rotationSpeed), 0)
-            end
-        end
-        wait(0.01)
-    end
-end
-
--- Fly System
-_G.FY = false
-function FY()
-    local player = game.Players.LocalPlayer
-    local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
-
-    while _G.FY and player.Character and humanoid do
-        humanoid.PlatformStand = false
-        humanoid.AutoRotate = false
-        humanoid.AutoRotateSpeed = 0
-        humanoid:Move(Vector3.new(0, 1, 0))
-        wait(0.1)
-    end
-end
-
--- Inf Jump System
-_G.IJ = false
-local function onJumpRequest()
-    game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
-end
-function IJ()
-    if _G.IJ then
-        disconnectIJ = userInputService.JumpRequest:Connect(onJumpRequest)
-    else
-        if disconnectIJ then
-            disconnectIJ:Disconnect()
-            disconnectIJ = nil
-        end
-    end
-end
-
--- Invade Match System
-local function TeleportToCoordinates()
-    if character then
-        local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-        humanoidRootPart.CFrame = CFrame.new(124.5, 25.99999857, 138.5)
-        game.Workspace.CurrentCamera.CFrame = CFrame.new(124.5, 4.99999857, 138.5, -0.351053178, 0, 0.936355531, 0, 1, 0, -0.936355531, 0, -0.351053178)
-    end
-end
-
--- Menu
-local Menu = Window:MakeTab({
-	Name = "Menu",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Menu:AddSection({
-	Name = "·-–— Tools —–-·"
-})
-Menu:AddButton({
-		Name = "FTFHAX",
-		Callback = function()
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/LeviTheOtaku/roblox-scripts/main/FTFHAX.lua",true))()
+-- Funções
+local function ComputerESP()
+	while _G.ComputerESP == true do
+		for _, map in pairs(workspace:GetChildren()) do
+			for _, computer in pairs(map:GetChildren()) do
+				if computer.Name == "ComputerTable" then
+					if computer:FindFirstChild("Highlight") then
+						if computer.Highlight.FillColor ~= computer.Screen.Color or computer.Highlight.OutlineColor ~= computer.Screen.Color then
+							computer.Highlight.FillColor = computer.Screen.Color
+							computer.Highlight.OutlineColor = computer.Screen.Color
+						elseif computer.Highlight.Enabled == false then
+							computer.Highlight.Enabled = true
+						end
+					else
+						local highlight = Instance.new("Highlight")
+						highlight.FillColor = computer.Screen.Color
+						highlight.OutlineColor = computer.Screen.Color
+						highlight.FillTransparency = 0.6
+						highlight.Adornee = computer
+						highlight.Parent = computer
+					end
+				end
+			end
 		end
-})
-Menu:AddToggle({
-    Name = "Anti Slow",
-    Default = false,
-    Callback = function(Value)
-        _G.AS = Value
-        AS()
-    end    
-})
-Menu:AddToggle({
-	Name = "Fullbright",
-	Default = false,
-	Callback = function(Value)
-        _G.FB = Value
-        FB()
-	end    
-})
-
--- Player
-local Self = Window:MakeTab({
-	Name = "Player",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Self:AddSection({
-	Name = "·-–— Movement —–-·"
-})
-Self:AddSlider({
-    Name = "Walk Speed",
-    Min = 16,
-    Max = 100,
-    Default = 16,
-    Color = Color3.fromRGB(255,150,0),
-    Increment = 1,
-    ValueName = "",
-    Callback = function(Value)
-        walkSpeed = Value
-    end    
-})
-Self:AddToggle({
-    Name = "Change Walk Speed",
-    Default = false,
-    Callback = function(Value)
-        _G.ChangeWalkSpeed = Value
-        if not Value then
-            local humanoid = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-            if humanoid and humanoid.WalkSpeed > 16 then
-                humanoid.WalkSpeed = 16
-            end
-        else
-            WalkSpeedLoop()
-        end
-    end    
-})
-local Section = Self:AddSection({
-	Name = "·-–— More —–-·"
-})
-Self:AddToggle({
-	Name = "Inf Jump",
-	Default = false,
-	Callback = function(Value)
-        _G.IJ = Value
-        IJ()
-	end    
-})
-Self:AddToggle({
-	Name = "No Clip",
-	Default = false,
-	Callback = function(Value)
-        _G.NC = Value
-        NC()
-	end    
-})
-Self:AddToggle({
-	Name = "Fly",
-	Default = false,
-	Callback = function(Value)
-				_G.FY = Value
-				NC()
-	end    
-})
-
--- Misc
-local Misc = Window:MakeTab({
-	Name = "Misc",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Section = Misc:AddSection({
-	Name = "·-–— Fun —–-·"
-})
-Misc:AddButton({
-	Name = "Invade match!",
-	Callback = function()
-        TeleportToCoordinates()
-    end
-})
-Misc:AddButton({
-	Name = "Chat Spy",
-	Callback = function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/ChatSpy.lua",true))()
-    end
-})
-Misc:AddToggle({
-    Name = "Spin",
-    Default = false,
-    Callback = function(Value)
-        _G.SP = Value
-        SP()
-    end    
-})
-
-OrionLib:Init()
+		wait(1)
+	end
+	if _G.ComputerESP == false then
+		for _, map in pairs(workspace:GetChildren()) do
+			for _, computer in pairs(map:GetChildren()) do
+				if computer.Name == "ComputerTable" then
+					if computer:FindFirstChild("Highlight") then
+						if computer.Highlight.Enabled == true then
+							computer.Highlight.Enabled = false
+						end
+					end
+				end
+			end
+		end
+	end
+end
