591b560d1b3f223024bcd9809366ed7875d95642
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-26 01:56:53 -0300

    update games/Flee The Facility.lua

--- games/Flee The Facility.lua
+++ games/Flee The Facility.lua
@@ -206,10 +206,9 @@ end
 local function Fullbright()
     while _G.Fullbright == true do
         game:GetService("Lighting").Brightness = 2
-        game:GetService("Lighting").ClockTime = 14
-        game:GetService("Lighting").FogEnd = 100000
+        game:GetService("Lighting").ClockTime = 12
+        game:GetService("Lighting").FogEnd = 0
         game:GetService("Lighting").GlobalShadows = false
-        game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
         wait(0.01)
     end
     game:GetService("Lighting").Brightness = 1
