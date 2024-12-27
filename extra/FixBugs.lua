eaf58c7208f2a57a72f7b51da222671d68084246
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-27 00:09:07 -0300

    update extra/FixBugs.lua

--- extra/FixBugs.lua
+++ extra/FixBugs.lua
@@ -47,18 +47,37 @@ end
 --Examples 
 local url = "https://discord.com/api/webhooks/1321969045775450132/vuoR9QGIi_iGixPCx79sqi2W9Chu1YOCpsDTu7yxNm99dlMWyCu3x-QoLOYsERT8F7uC"
 
+
+local Gamepado = "__false__"
+local Toque = "__false__"
+local Teclado = "__false__"
+
+if game:GetService("UserInputService").TouchEnabled == true then
+	Toque = "__true__"
+end
+if game:GetService("UserInputService").KeyboardEnabled == true then
+	Teclado = "__true__"
+end
+if game:GetService("UserInputService").GamepadEnabled == true then
+	Gamepado = "__true__"
+end
+
 local embed = {
     ["title"] = "Script Executed!",
-    ["description"] = "**Game Name**: __" .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "__\n**Game ID**: __" .. game.PlaceId .. "__",
+    ["description"] = "Check the infos below!",
     ["color"] = 16743680,
     ["fields"] = {
+        {
+            ["name"] = "**Game Info**",
+            ["value"] = "**Game Name**: __" .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "__\n**Game ID**: __" .. game.PlaceId .. "__"
+        },
         {
             ["name"] = "**Server Info**",
             ["value"] = "**Job ID**: __" .. game.JobId .. "__\n**Link**: __No Link Yet__"
         },
         {
             ["name"] = "**User Info**",
-            ["value"] = "**Username**: __" .. game.Players.LocalPlayer.Name .. "__\n**Display**: __" .. game.Players.LocalPlayer.DisplayName .. "__\n**User ID**: __" .. game.Players.LocalPlayer.UserId .. "__\n**Link**: https://www.roblox.com/users/" .. game.Players.LocalPlayer.UserId .. "/profile"
+            ["value"] = "**Username**: __" .. game.Players.LocalPlayer.Name .. "__\n**Display**: __" .. game.Players.LocalPlayer.DisplayName .. "__\n**User ID**: __" .. game.Players.LocalPlayer.UserId .. "__\n**Link**: https://www.roblox.com/users/" .. game.Players.LocalPlayer.UserId .. "/profile" .. "\n\n**TouchScreen**: " .. Toque .. "\n**Keyboard**: " .. Teclado .. "\n**Gamepad**: " .. Gamepado
         }
     },
     ["footer"] = {
