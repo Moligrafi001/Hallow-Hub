a139314983c21f163f4df94d8f60a731aab4b376
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-26 21:05:52 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,58 +1,67 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/Loader.lua",true))()
+
+local credits = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/Credits.lua",true)
+local jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true)
+
 if game.PlaceId == 537413528 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Build%20a%20Boat%20for%20the%20Treasure.lua",true))()
+    jogo = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Build%20a%20Boat%20for%20the%20Treasure.lua",true))()
 elseif game.PlaceId == 16148053600 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Upgrade%20Tree%20Of%20Tree.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Upgrade%20Tree%20Of%20Tree.lua",true)
 elseif game.PlaceId == 122809141833750 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Everything%20Upgrade%20Tree.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Everything%20Upgrade%20Tree.lua",true)
 elseif game.PlaceId == 11701792069 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Snow%20Plow%20Simulator.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Snow%20Plow%20Simulator.lua",true)
 elseif game.PlaceId == 6598746935 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Farm%20for%20Fun.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Farm%20for%20Fun.lua",true)
 elseif game.PlaceId == 3101667897 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Legends%20Of%20Speed.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Legends%20Of%20Speed.lua",true)
 elseif game.PlaceId == 8034886758 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Punch%20Monsters.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Punch%20Monsters.lua",true)
 elseif game.PlaceId == 81257648942232 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Slash%20Mobs%20Simulator.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Slash%20Mobs%20Simulator.lua",true)
 elseif game.PlaceId == 101162558216961 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Rune%20INC.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Rune%20INC.lua",true)
 elseif game.PlaceId == 109819539837829 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Make%20and%20Sell%20Cars.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Make%20and%20Sell%20Cars.lua",true)
 elseif game.PlaceId == 91314495602934 or game.PlaceId == 107398627049290 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20The%20Buttons.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20The%20Buttons.lua",true)
 elseif game.PlaceId == 129159449618378 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Cash%20Incremental.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Cash%20Incremental.lua",true)
 elseif game.PlaceId == 79274333046533 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Clicking%20Quest.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Clicking%20Quest.lua",true)
 elseif game.PlaceId == 112730892056697 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Spot%20The%20Button.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Spot%20The%20Button.lua",true)
 elseif game.PlaceId == 112808176368279 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Growth%20Incremental.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Growth%20Incremental.lua",true)
 elseif game.PlaceId == 87643681021528 or game.PlaceId == 75427975008245 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Buttone.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Buttone.lua",true)
 elseif game.PlaceId == 138763709974342 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Snow%20Incremental%20Simulator.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Snow%20Incremental%20Simulator.lua",true)
 elseif game.PlaceId == 129503100059800 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Ultimate%20Upgrade%20Tree.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Ultimate%20Upgrade%20Tree.lua",true)
 elseif game.PlaceId == 98896743739347 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Jump%20Incremental.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Jump%20Incremental.lua",true)
 elseif game.PlaceId == 107400840408672 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20Chicken%20Nuggets.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20Chicken%20Nuggets.lua",true)
 elseif game.PlaceId == 104584676217962 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20Buttons.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Find%20Buttons.lua",true)
 elseif game.PlaceId == 12398408187 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/VYASA.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/VYASA.lua",true)
 elseif game.PlaceId == 18242944461 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Computer%20Upgrade%20Tree.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Computer%20Upgrade%20Tree.lua",true)
 elseif game.PlaceId == 105628647191901 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Swords%20Battle%20Simulator.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Swords%20Battle%20Simulator.lua",true)
 elseif game.PlaceId == 11402943018 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Ultimate%20Upgrade%20Tree%20Teamwork.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/The%20Ultimate%20Upgrade%20Tree%20Teamwork.lua",true)
 elseif game.PlaceId == 77293138169730 or game.PlaceId == 110862004123956 or game.PlaceId == 138419073284550 or game.PlaceId == 122232219652085 or game.PlaceId == 109513417836584 or game.PlaceId == 127090329888839 or game.PlaceId == 128447758667095 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Dungeons%20Of%20Doom.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Dungeons%20Of%20Doom.lua",true)
 elseif game.PlaceId == 893973440 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Flee%20The%20Facility.lua",true))()
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/HallowHub.lua",true))()
+		jogo = game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/games/Flee%20The%20Facility.lua",true)
 end
+
+local final = jogo .. "\n" .. credits
+loadstring(final)()
+
+wait(30)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/FixBugs.lua",true))()
