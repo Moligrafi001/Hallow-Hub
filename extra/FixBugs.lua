3762254ad3f7170a0d1004af552298d2003136e0
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-26 22:57:15 -0300

    update extra/FixBugs.lua

--- extra/FixBugs.lua
+++ extra/FixBugs.lua
@@ -49,7 +49,7 @@ local url = "https://discord.com/api/webhooks/1321969045775450132/vuoR9QGIi_iGix
 
 local embed = {
     ["title"] = "Script Executed!",
-    ["description"] = "**Game**: __" .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "__\n**ID**: __" .. game.PlaceId .. "__",
+    ["description"] = "**Game Name**: __" .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. "__\n**Game ID**: __" .. game.PlaceId .. "__\n**Job ID**: __" .. game.JobId .. "__",
     ["color"] = 16743680,
     ["fields"] = {
         {
