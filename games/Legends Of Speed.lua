cd829c7923f43edb8c689854194f013cd2d8c185
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2024-12-31 16:48:12 -0300

    update games/Legends Of Speed.lua

--- games/Legends Of Speed.lua
+++ games/Legends Of Speed.lua
@@ -8,6 +8,60 @@ local Window = Rayfield:CreateWindow({
    Theme = "Amethyst"
 })
 
+-- Movement
+local WalkSpeedText = 16
+local JumpPowerText = 50
+_G.SetWalkSpeed = false
+_G.SetJumpPower = false
+_G.InfJump = false
+_G.NoClip = false
+local function SetWalkSpeed()
+	while _G.SetWalkSpeed == true do
+		if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed ~= WalkSpeedText then
+			game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = WalkSpeedText
+		end
+		wait(0.01)
+	end
+	if _G.SetWalkSpeed == false then
+		game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = 16
+	end
+end
+local function SetJumpPower()
+	while _G.SetJumpPower == true do
+		if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower ~= JumpPowerText then
+			game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = JumpPowerText
+		end
+		wait(0.01)
+		end
+	if _G.SetJumpPower == false then
+		game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = 50
+	end
+end
+local function InfJump()
+	while _G.InfJump == true do
+		game:GetService("UserInputService").JumpRequest:connect(function()
+			if _G.InfJump == true then
+				game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
+			end
+		end)
+		wait(0.1)
+	end
+end
+local function NoClip()
+	while _G.NoClip == true do
+		for _, part in ipairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+			if part:IsA("BasePart") then
+				if _G.NoClip then
+					part.CanCollide = false
+				else
+					part.CanCollide = true
+				end
+			end
+		end
+		wait(0.1)
+	end
+end
+
 -- Valores
 _G.AutoXP = false
 _G.FarmS = false
@@ -66,6 +120,13 @@ local function AutoHoop()
 		wait(0.01)
 	end
 end
+local function UnlockTPs()
+	for _, part in pairs(workspace.areaCircles:GetChildren()) do
+		if part.Name == "areaCircle" then
+			part.requiredAmount.Value = 0
+		end
+	end
+end
 
 -- Menu
 local Menu = Window:CreateTab("Main", "home")
@@ -115,6 +176,77 @@ local Toggle =  Menu:CreateToggle({
 -- Animal
 local AnimalTab = Window:CreateTab("Pets", "paw-print")
 local Section = AnimalTab:CreateSection("Coming soon... :D")
+
+-- Teleport
+local TPsTab = Window:CreateTab("Teleport", "Shell")
+local Section = TPsTab:CreateSection("Auto Farm")
+local Button = TPsTab:CreateButton({
+   Name = "Unlock All Portals",
+   Callback = function()
+       UnlockTPs()
+    end,
+})
+
+-- Movement
+local MoveTab = Window:CreateTab("Movement", "chevrons-up")
+local Section = MoveTab:CreateSection("Walk")
+local Input = MoveTab:CreateInput({
+   Name = "Player Walk Speed",
+   CurrentValue = "",
+   Flag = "WalkSpeedInput",
+   PlaceholderText = "Default Walk Speed = 16",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(Text)
+   	WalkSpeedText = Text
+   end,
+})
+local Toggle = MoveTab:CreateToggle({
+   Name = "Toggle Walk Speed",
+   CurrentValue = false,
+   Flag = "WalkSpeedToggle", 
+   Callback = function(Value)
+   	_G.SetWalkSpeed = Value
+   	SetWalkSpeed()
+   end,
+})
+local Toggle = MoveTab:CreateToggle({
+   Name = "No Clip",
+   CurrentValue = false,
+   Flag = "NoClipToggle",
+   Callback = function(Value)
+   	_G.NoClip = Value
+   	NoClip()
+   end,
+})
+local Section = MoveTab:CreateSection("Jump")
+local Input = MoveTab:CreateInput({
+   Name = "Player Jump Power",
+   CurrentValue = "",
+   Flag = "JumpPowerInput",
+   PlaceholderText = "Default Jump Power = 50",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(Text)
+   	JumpPowerText = Text
+   end,
+})
+local Toggle = MoveTab:CreateToggle({
+   Name = "Toggle Jump Power",
+   CurrentValue = false,
+   Flag = "JumpPowerToggle",
+   Callback = function(Value)
+   	_G.SetJumpPower = Value
+   	SetJumpPower()
+   end,
+})
+local Toggle = MoveTab:CreateToggle({
+   Name = "Inf Jump",
+   CurrentValue = false,
+   Flag = "InfJumpToggle",
+   Callback = function(Value)
+   	_G.InfJump = Value
+   	InfJump()
+   end,
+})
 
 -- Credits.
 local Credits = Window:CreateTab("Credits", "info")
