68703b6fd1811df6368e5266f97ad92ebff1ed83
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-03-21 10:24:26 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,6 @@
-local function TestCode(caminho)
-  local httrest = http_request or request or (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request)
-  local response = httrest({
-    Url = "https://hallow-hub.squareweb.app/api/load/" .. game:GetService("HttpService"):UrlEncode(caminho),
-    Method = "GET",
-    Headers = {
-      ["pass"] = "SuperSecretHallowHubKey090609"
-    }
-  })
-  loadstring(game:GetService("HttpService"):JSONDecode(response.Body)["script"])()
-end
-
-TestCode("Key System.lua")
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Hallow Hub",
+	Text = "Under Maintenance\nhttps://discord.gg/AESCuek87s",
+	Icon = "",
+	Duration = math.huge
+})
