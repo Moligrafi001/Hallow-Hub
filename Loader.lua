24131385fdaa661115ac85247091dc82525d0d2e
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-20 00:57:51 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,15 @@
-local function TestCode(caminho)
-   local httrest = http_request or request or (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request)
-   local response = httrest({
-     Url = "https://hallow-hub.squareweb.app/api/load/" .. game:GetService("HttpService"):UrlEncode(caminho),
-     Method = "GET",
-     Headers = {
-       ["pass"] = "SuperSecretHallowHubKey090609"
-     }
-   })
-   loadstring(game:GetService("HttpService"):JSONDecode(response.Body)["script"])()
- end
- 
- TestCode("Loader.lua")
+local luaujson = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/luaujson/main/library.lua"))()
+
+luaujson:Start()
+
+local function LoadScript(path)
+  loadstring(luaujson:Parse({
+    Url = "https://hallow-hub.squareweb.app/api/load/" .. game:GetService("HttpService"):UrlEncode(path),
+    Method = "GET",
+    Headers = {
+      ["pass"] = "SuperSecretHallowHubKey090609"
+    }
+  }, "script"))()
+end
+
+LoadScript("Loader.lua")
